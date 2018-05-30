0c503d2415c3a2e2a967c9f6902a37f0328cfdf6
vladovoev@yahoo.co.uk <vladovoev@yahoo.co.uk>
2018-05-30 15:59:25 +0300

    fetching orders from db

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -2,10 +2,31 @@ import React from 'react';
 import classes from './Order.css'
 
 const Order = (props) => {
+
+    const ingredients = [];
+    for (let ingName in props.ingredients) {
+        ingredients.push({
+            name: ingName,
+            amount: props.ingredients[ingName]
+        })
+    }
+
+    const ingOutput = ingredients.map(ig => {
+        return <span
+            style={{
+                textTransform: 'capitalize',
+                display: 'inline-block',
+                margin: '0 8px',
+                border: '1px solid black',
+                padding: '5px'
+            }}
+
+            key={ig.name}>{ig.name} ({ig.amount})</span>
+    })
     return (
         <div className={classes.Order}>
-            <p>Ingredients:</p>
-            <p>Price <strong>5.45$</strong></p>
+            <p>Ingredients:{ingOutput}</p>
+            <p>Price <strong>USD ${props.price}</strong></p>
         </div>
     );
 };
